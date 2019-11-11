0e77bd72c0790291418500fdd649ba6d814e3e8f
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-11 15:44:43 +0700

    02 Core Java essentials for Selenium: 009 Java Methods - Part 2

--- CoreJava/src/com/gekif/LearningMethods.java
+++ CoreJava/src/com/gekif/LearningMethods.java
@@ -3,13 +3,20 @@ package com.gekif;
 public class LearningMethods {
 
     int i; // Declaration, initialize
+    int j;
+
+    public void show() {
+        int a;
+        int b;
+        String c;
+    }
 
     // Return type --> dataType of the value returned by method
 
     // Keyword --> return
 
     // Display
-    public static void display() { // Declare --> define
+    public int display(int k, String a, char c, float f, boolean b) { // Declare --> define
     // Starts
 
         /**
@@ -18,18 +25,24 @@ public class LearningMethods {
          *
          */
 
+        int j = 100;
         System.out.println("Inside Display Method");
 
 //        String x = "Hello";
 //        return x; // Last Statement
 
+        return 1;
     } // Ends
 
 
     public static void main(String[] args) {
 
-        System.out.println("Hi How Are You?");
-        display();
+        LearningMethods learn = new LearningMethods();
+        learn.show();
+
+        LearningMethods learn1 = new LearningMethods();
+        learn1.show();
+
 
     }
 }
