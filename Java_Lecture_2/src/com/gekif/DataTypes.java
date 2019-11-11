127da63684a7d0b67b4a2c29bc20ec874a2a8928
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-11 14:56:10 +0700

    02 Core Java essentials for Selenium: 006 Basic Java programming and features - Part 1

---
+++ Java_Lecture_2/src/com/gekif/DataTypes.java
@@ -0,0 +1,25 @@
+package com.gekif;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int i = 1234567890; // int --> integer
+        float f = 1.233434234234234324324f;
+        long l = 1234567890134343l;
+        double d = 1.232323348284;
+        boolean b = true;
+        char c = 'a';
+        String s = "Java"; // Class in Java
+
+        DataTypes dt;
+        Test t;
+
+        int batmanBegins; // Camel Case Variable
+        int theDarkKnight;
+        int theDarkKningRises;
+
+
+
+    }
+}
---
+++ Java_Lecture_2/src/com/gekif/Test.java
@@ -0,0 +1,16 @@
+package com.gekif;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Test t = new Test();
+        System.out.println(t);
+
+        t = null;
+
+        Test t1 = t;
+
+        Home h = new Home();
+    }
+}
