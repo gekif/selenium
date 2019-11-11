4518993c18538885e2b0c39d31cc9385f4e8d891
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-11 15:07:20 +0700

    02 Core Java essentials for Selenium: 006 Basic Java programming and features - Part 2

--- Java_Lecture_2/src/com/gekif/DataTypes.java
+++ Java_Lecture_2/src/com/gekif/DataTypes.java
@@ -19,7 +19,47 @@ public class DataTypes {
         int theDarkKnight;
         int theDarkKningRises;
 
+        String abc = new String();
+        abc = "Hello";
 
+        String abc1 = "Hello";
+
+
+        String s1 = "Way";
+        String s2 = "2";
+        String s3 = "Automation";
+
+        String s4 = s1 + s2 + s3;
+
+        // String + String --> Concatenated String
+        System.out.println(s4);
+        System.out.println(s1 + s2 + s3);
+        System.out.println("Way" + "2" + "Automation");
+
+
+        // int + int --> int
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 + num2 + " after");
+        System.out.println("before " + num1 + num2 + " after");
+        System.out.println("before " + (num1 + num2) + " after");
+
+
+        /**
+         * 12 + 45 // Operands
+         *
+         * Unary Operator --> Single Operand
+         * Binary Operator --> Two Operand
+         * Ternary Operator --> Three Operand
+         */
+
+        System.out.println(34 == 78);
+        System.out.println(34 <= 78);
+        System.out.println(true && false);
+        System.out.println((123 == 123) && (10 == 10));
+        System.out.println((123 == 123) || (10 == 10));
 
     }
 }
