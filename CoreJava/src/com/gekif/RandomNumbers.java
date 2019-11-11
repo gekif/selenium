a3330eecf38d70abd04f1b2d14a594e0c0293ca9
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-11 16:16:24 +0700

    02 Core Java essentials for Selenium: 009 Java Methods - Part 5

---
+++ CoreJava/src/com/gekif/RandomNumbers.java
@@ -0,0 +1,13 @@
+package com.gekif;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+
+        // Type Casting -->
+
+        int num = (int) (Math.random() * 100);
+        System.out.println(num);
+
+    }
+}
